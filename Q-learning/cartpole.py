7bb592915f72523e7c797f99098350b3725cdb7e
romansmit <romansmit@users.noreply.github.com>
2019-03-02 12:16:16 +0100

    - added minimal training example to main - added training function that shows a progress bar

--- Q-learning/cartpole.py
+++ Q-learning/cartpole.py
@@ -6,16 +6,16 @@ from keras.optimizers import Adam
 from collections import deque
 import random
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
-class CartpoleQAgent:
+class QAgent:
     """Q learning agent.
     Holds the model for the Q-function and the memory for memory replay.
     Contains a function to fit the model from memory.
     Contains a function to save state-action-reward-state tuples to the memory.
     Contains a function to choose an action given a state
     """
-
     def __init__(self, memory_size=5000, discount=.98):
 
         self.action_dim = 2  # action 0: push left; action 1: push right
@@ -34,10 +34,10 @@ class CartpoleQAgent:
         noise = np.random.normal(loc=0, scale=rand, size=self.action_dim)
         return np.argmax(qs + noise)
 
-    def remember(self, st1, ac, rew, st2, don):
-
+    def remember_episode(self, st1, ac, rew, st2, don):
         """Commit series to memory.
         Selective memory: only memorize samples where prediction was worse than average.
+        This means the actual Q-values must be known.
         """
         actual_qs = self.calc_q(rew)
         predicted_qs = self.Q_model_predict.predict(st1) * np.array(ac)
@@ -69,10 +69,8 @@ class CartpoleQAgent:
         return qs
 
     def fit_from_memory(self, epochs=3, batch_size=16):
-
         """Fit the Q model on random data from the memory
         """
-
         sample_size = min(batch_size, len(self.memory))
         for i in range(epochs):
             sample = random.sample(self.memory, sample_size)
@@ -85,11 +83,9 @@ class CartpoleQAgent:
             self.Q_model_train.train_on_batch([st1, ac], q_target)
 
     def build_q_model(self, hidden_layer_sizes=(40, 40)):
-
         """build the Q model.
         Returns one model for prediction and one for training.
         """
-
         inp_st = layers.Input(shape=(self.state_dim,))
         prev = inp_st
         for n in hidden_layer_sizes:
@@ -110,11 +106,12 @@ class CartpoleQAgent:
         return model_train, model_predict
 
 
-class CartpoleSession:
-    """provides simple to use functions for training and testing
+class Session:
+    """holds the agent
+    provides simple to use functions for training, testing and plotting
     """
     def __init__(self):
-        self.agent = CartpoleQAgent()
+        self.agent = QAgent()
 
         self.achievements = []
         self.new_mems = []
@@ -122,9 +119,18 @@ class CartpoleSession:
 
         self.total_train_time = 0
 
+    def training_cycle(self, epochs=5, epoch_time=10000, plotting=False):
+        """execute multiple epochs of training.
+        between the epochs, a progress bar is updated and, if plotting=True, plots are shown
+        """
+        for _ in tqdm(range(epochs)):
+            self.train(approx_train_time=epoch_time)
+            if plotting:
+                self.plot_data()
+        print('Total train time: {}'.format(self.total_train_time))
+        print('Number of completed episodes: {}'.format(len(self.achievements)))
+
     def plot_data(self):
-        print('Total train time: {}\nNumber of completed episodes: {}'.format(self.total_train_time,
-                                                                              len(self.achievements)))
         plt.figure(figsize=(16, 4))
         plt.subplot(1, 3, 1)
         plt.plot(self.achievements, 'r.')
@@ -143,7 +149,6 @@ class CartpoleSession:
         if 'render' is True, render the game to a separate window.
         return a list of the total reward achieved in each round.
         """
-
         achievements = []
         env = gym.make('CartPole-v1')
 
@@ -169,10 +174,11 @@ class CartpoleSession:
         env.close()
         return achievements
 
-    def train(self, approx_train_time=5000):
-        """train 'agent' for 'train_time' time steps of CartPole-v1.
+    def train(self, approx_train_time=10000):
+        """train 'agent' for 'approx_train_time' time steps of CartPole-v1.
+        the time is only approximate because training will only end after completing an episode,
+        this is needed for selective memory.
         """
-
         action_list = np.identity(self.agent.action_dim)
 
         env = gym.make('CartPole-v1')
@@ -212,8 +218,8 @@ class CartpoleSession:
 
                 # only learn from failed episodes because we dont know the future reward of the last state
                 if cum_reward < 500:
-                    n_mems = self.agent.remember(np.array(st1), np.array(ac),
-                                                 np.array(rew), np.array(st2), np.array(don))
+                    n_mems = self.agent.remember_episode(np.array(st1), np.array(ac),
+                                                         np.array(rew), np.array(st2), np.array(don))
                     self.agent.fit_from_memory(epochs=4, batch_size=64)
                 else:
                     n_mems = 0
@@ -235,4 +241,17 @@ class CartpoleSession:
         self.new_mems += new_mems
         self.total_train_time += t
         self.variances.append(np.sqrt(self.agent.avg_mse))
-        return achievements, variances, new_mems, t
+
+
+if __name__ == "__main__":
+    """Minimal example
+    trains the agent, and afterwards renders how the agent plays one round
+    lastly, data collected during training is plotted
+    """
+    epochs = 10
+    epoch_time = 10000
+    print("training the agent for {} epochs of approximately {} time steps each...".format(epochs, epoch_time))
+    sess = Session()
+    sess.training_cycle(epochs, epoch_time, plotting=False)
+    sess.play()
+    sess.plot_data()
